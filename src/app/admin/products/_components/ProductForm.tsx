7c2c66d7d853363b67951603e3ed50896d599dcf
RanaKashyap <kashyaprana202@gmail.com>
2024-04-13 12:59:25 -0400

    first changes

---
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+export function ProductForm() {
+  return (
+    <form className="space-y-8">
+      <div className="space-y-2">
+        <Label htmlFor="name">Name</Label>
+        <Input type="text" id="name" name="name" required />
+      </div>
+      {/* 46:30 */}
+    </form>
+  );
+}
